6e3c8c871bd437454f9c8a66a19c73962aee8057
YZBruh <yagizzengin73@gmail.com>
2024-04-06 09:27:53 +0300

    a01core: switch makefiles omni to twrp

--- twrp_a01core.mk
+++ twrp_a01core.mk
@@ -1,11 +1,11 @@
 # Call makefiles
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/samsung/a01core/device.mk)
 
 # Device
 PRODUCT_DEVICE := a01core
-PRODUCT_NAME := omni_a01core
+PRODUCT_NAME := twrp_a01core
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A013M
 PRODUCT_MANUFACTURER := samsung
