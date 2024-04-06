bf2452739ed03c753dc2f19afa58e433dcf72101
YZBruh <yagizzengin73@gmail.com>
2024-04-06 09:28:55 +0300

    a01core: switch makefiles omni to twrp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,6 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_a01core.mk
+    $(LOCAL_DIR)/twrp_a01core.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_a01core-user \
-    omni_a01core-userdebug \
-    omni_a01core-eng
+    twrp_a01core-userdebug \
+    twrp_a01core-eng
